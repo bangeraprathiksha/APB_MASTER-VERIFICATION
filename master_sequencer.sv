8a3077acf14cfa4d26433befd608e1c8c688176d
Preathiksha <109804004+bangeraprathiksha@users.noreply.github.com>
2025-12-07 20:18:13 +0530

    Create master_sequencer.sv

---
+++ master_sequencer.sv
@@ -0,0 +1,9 @@
+class master_sequencer extends uvm_sequencer #(master_seq_item);
+
+  `uvm_component_utils(master_sequencer)
+
+  function new(string name = "master_sequencer", uvm_component parent);
+    super.new(name, parent);
+  endfunction
+
+endclass
